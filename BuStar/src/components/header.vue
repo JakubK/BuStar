5247612cf24a4c6dbeb26003c2f52ae31ddf4d4c
Frisk <furrtrex43@protonmail.ch>
2018-10-08 18:59:13 +0200

    Dodano header
    Ogólne zapewne jest coś okropnie źle, idk, poprawię później, CSS jest całkiem ew a co do vue to dopiero się zapoznaje głębiej

---
+++ BuStar/src/components/header.vue
@@ -0,0 +1,12 @@
+ <template>
+    <div class="project-nav">
+    <div class="project-name-header">BuStar</div>
+      <div class="main-nav">
+        <span class="nav-item">Home</span>
+        <span class="nav-item">About</span>
+      </div>
+      <div class="right"></div>
+    </div>
+</template>
+
+<style lang="scss" src="./header.scss" scoped></style>
