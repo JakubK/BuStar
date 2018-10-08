79315b58c2b208d402847e59e6a7dfdcbc1465e8
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2018-10-08 18:42:17 +0200

    Implement and register DownloadTristarDataTask that prints message every 6 hours

--- BuStarAPI/Startup.cs
+++ BuStarAPI/Startup.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BuStarAPI.Extensions;
+using BuStarAPI.Scheduling;
+using BuStarAPI.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +29,14 @@ namespace BuStarAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSingleton<IScheduledTask, DownloadTristarDataTask>();
+            services.AddScheduler((sender, args)=>
+            {
+                Console.Write(args.Exception.Message);
+                args.SetObserved();
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
