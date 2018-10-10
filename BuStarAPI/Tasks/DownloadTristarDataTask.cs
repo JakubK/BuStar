56945d7b0a0d125a8433c4761987cfe033a534fe
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2018-10-10 16:59:26 +0200

    Inject IRepository and IDataParseService to DownloadTristarDataTask and use them to drop collections and parse downloaded data

--- BuStarAPI/Tasks/DownloadTristarDataTask.cs
+++ BuStarAPI/Tasks/DownloadTristarDataTask.cs
@@ -3,7 +3,9 @@ using System.Net;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
+using BuStarAPI.Repository;
 using BuStarAPI.Scheduling;
+using BuStarAPI.Services;
 
 namespace BuStarAPI.Tasks
 {
@@ -11,8 +13,13 @@ namespace BuStarAPI.Tasks
   {
     public string Schedule => "* */24 * * *";
 
-    public DownloadTristarDataTask()
+    private IRepository repository;
+    private IDataParseService dataParseService;
+
+    public DownloadTristarDataTask(IRepository repositoryParam, IDataParseService dataParseServiceParam)
     {
+      this.repository = repositoryParam;
+      this.dataParseService = dataParseServiceParam;
     }
 
     public async Task ExecuteAsync(CancellationToken cancellationToken)
@@ -21,6 +28,11 @@ namespace BuStarAPI.Tasks
       {
         var stops = client.DownloadString(new Uri("http://91.244.248.19/dataset/c24aa637-3619-4dc2-a171-a23eec8f2172/resource/cd4c08b5-460e-40db-b920-ab9fc93c1a92/download/stops.json"));
         var routes = client.DownloadString(new Uri("http://91.244.248.19/dataset/c24aa637-3619-4dc2-a171-a23eec8f2172/resource/4128329f-5adb-4082-b326-6e1aea7caddf/download/routes.json"));
+
+        //drop collections and send JSON to parsing service
+        repository.Clear();
+        dataParseService.ParseBuses(routes);
+        dataParseService.ParseStops(stops);
 
         await Task.FromResult(0);
       }
