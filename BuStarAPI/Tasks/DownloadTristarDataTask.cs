b3218a5dc6f2516532ab041f3895404aa69c51aa
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2018-10-10 15:48:19 +0200

    Configure DownloadTristarDataTask to download information about stops and routes every day

--- BuStarAPI/Tasks/DownloadTristarDataTask.cs
+++ BuStarAPI/Tasks/DownloadTristarDataTask.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Net;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using BuStarAPI.Scheduling;
@@ -6,7 +9,7 @@ namespace BuStarAPI.Tasks
 {
   public class DownloadTristarDataTask : IScheduledTask
   {
-    public string Schedule => "* */6 * * *";
+    public string Schedule => "* */24 * * *";
 
     public DownloadTristarDataTask()
     {
@@ -14,8 +17,13 @@ namespace BuStarAPI.Tasks
 
     public async Task ExecuteAsync(CancellationToken cancellationToken)
     {
-      await Task.Delay(0);
-      System.Diagnostics.Debug.WriteLine("CRON TASK EXECUTED");
+      using (var client = new WebClient())
+      {
+        var stops = client.DownloadString(new Uri("http://91.244.248.19/dataset/c24aa637-3619-4dc2-a171-a23eec8f2172/resource/cd4c08b5-460e-40db-b920-ab9fc93c1a92/download/stops.json"));
+        var routes = client.DownloadString(new Uri("http://91.244.248.19/dataset/c24aa637-3619-4dc2-a171-a23eec8f2172/resource/4128329f-5adb-4082-b326-6e1aea7caddf/download/routes.json"));
+
+        await Task.FromResult(0);
+      }
     }
   }
 }
