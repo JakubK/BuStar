8ca687e392eb4657c0a577ad45e48d53ee3f31eb
Frisk <furrtrex43@protonmail.ch>
2018-10-15 16:39:38 +0200

    Made some elements of about page?
    It has a lot of issues with it being responsive. TODO later.

--- BuStar/src/components/About/About.vue
+++ BuStar/src/components/About/About.vue
@@ -1,6 +1,22 @@
  <template>
     <div class="main_body">
-
+      <div class="about_section">
+        <h2>About BuStar</h2>
+          <span>BuStar is a project focused on making the city communication easier by replacing the often inaccurate paper departure plans with updated in real time easy to use web application. It has been made in two weeks by four passionate people.</span>
+      </div>
+      <div class="authors">
+          <h2>Authors</h2>
+          <div class="author-boxes">
+            <div class="author">
+            </div>
+            <div class="author">
+            </div>
+            <div class="author">
+            </div>
+            <div class="author">
+            </div>
+          </div>
+        </div>
     </div>
  </template>
 
