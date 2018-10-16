e6300278419e39594aaf374cc3d3b7f53fd9f18c
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2018-10-16 20:26:46 +0200

    Add Basic Preloader for Home component

--- BuStar/src/components/Home/Home.vue
+++ BuStar/src/components/Home/Home.vue
@@ -22,6 +22,9 @@
           <td>{{busInfo.estimatedTime}}</td>
          </tr>
         </template>
+        <div class="preloader" v-if="searching == 1">
+          Searching for buses...
+        </div>
     </table>
   </div>
 </template>
@@ -44,7 +47,8 @@
         busStops: null,
         stopDatas: [],
         busStopsTips: [],
-        searchInput: ''
+        searchInput: '',
+        searching: -1
       }
     },
     mounted() {
@@ -59,6 +63,7 @@
 
     methods: {
       searchSubmit() {
+        this.searching = 1;
         return axios.get("https://localhost:5001/api/bustar/stopdata/" + this.searchInput.replace('/', '*'))
           .then((response) => {
             this.stopDatas = response.data;
@@ -71,6 +76,7 @@
         {
           this.showTable=true;
           this.showTips=false;
+          this.searching = 0;
         })
       },
       inputChange() {
