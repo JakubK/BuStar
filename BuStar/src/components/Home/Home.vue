928e0f0213c763992a3b4839151a2612f628b7ad
DESKTOP-7ABJ9TM\mateu <mateuszh.spam@gmail.com>
2018-10-16 21:34:27 +0200

    Fixed input style issue when user type something that's not in stopDatas

--- BuStar/src/components/Home/Home.vue
+++ BuStar/src/components/Home/Home.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="main_body">
-     <p>BuStar</p>
-        <form @submit.prevent="createTable(busStopsTips[0])">
+    <p>BuStar</p>
+    <form @submit.prevent="createTable(busStopsTips[0])">
 
-      <input v-bind:class="inputClass" v-model="searchInput" @input="inputChange" autocomplete="off" autocorrect="off" autocapitalize="off"
-      spellcheck="false" type="text" autofocus placeholder="Start typing your stop's name">
+      <input v-bind:class="inputClass" v-model="searchInput" @input="inputChange" autocomplete="off" autocorrect="off"
+        autocapitalize="off" spellcheck="false" type="text" autofocus placeholder="Start typing your stop's name">
       <input hidden type="submit">
-     <!-- <ul v-if="showTips" class="tipsUl">
+      <!-- <ul v-if="showTips" class="tipsUl">
         <li v-on:mouseover="searchInput=stops" v-on:click="createTable" class="tipsLi" v-for="stops in busStopsTips" :key="stops">{{ stops }}</li>
       </ul>-->
-            <span v-if="showTips" v-on:click="createTable(stops)" v-bind:class="tipsListClass" v-for="stops in busStopsTips" :key="stops">{{ stops }}</span>   
+      <span v-if="showTips" v-on:click="createTable(stops)" v-bind:class="tipsListClass" v-for="stops in busStopsTips"
+        :key="stops">{{ stops }}</span>
       <table v-if="showTable">
         <tr>
           <th>Bus Line</th>
@@ -18,12 +19,12 @@
           <th>Estimated Arrival Time</th>
         </tr>
         <template v-for="(stopInfo) in stopDatas.stopInfos">
-         <tr :key="busInfo.routeID + busInfo.headsign + busInfo.theoreticalTime" v-for="(busInfo) in stopInfo.busInfos">
-          <td>{{busInfo.routeID}}</td>
-          <td>{{busInfo.headsign}}</td>
-          <td>{{busInfo.theoreticalTime}}</td>
-          <td>{{busInfo.estimatedTime}}</td>
-         </tr>
+          <tr :key="busInfo.routeID + busInfo.headsign + busInfo.theoreticalTime" v-for="(busInfo) in stopInfo.busInfos">
+            <td>{{busInfo.routeID}}</td>
+            <td>{{busInfo.headsign}}</td>
+            <td>{{busInfo.theoreticalTime}}</td>
+            <td>{{busInfo.estimatedTime}}</td>
+          </tr>
         </template>
       </table>
       <div class="preloader" v-if="searching == 1">
@@ -59,7 +60,7 @@
       axios.get("https://localhost:5001/api/bustar/buses")
         .then((response) => {
           this.busStops = response.data,
-          console.log(this.busStops)
+            console.log(this.busStops)
         })
         .catch(error => {
           console.log(error.response)
@@ -75,34 +76,37 @@
             return response.data;
           })
       },
-      createTable(stop){
+      createTable(stop) {
         this.inputClass = 'inputStyle';
-        this.showTips=false;
-        this.searchInput=stop;
-        this.searchSubmit().then(() =>
-        {
-          this.showTable=true; 
+        this.showTips = false;
+        this.searchInput = stop;
+        this.searchSubmit().then(() => {
+          this.showTable = true;
           this.searching = 0;
         })
       },
       inputChange() {
-        if (this.searchInput != '') {
-          this.showTips = true;
-          this.showTable = false;
-          this.inputClass = 'inputStyleActive';
-        } else {
-          this.showTips = false;
-          this.inputClass = 'inputStyle';
-        }
         this.busStopsTips = [];
         for (this.i = 0, this.tipsPostion = 0; this.i < Object.keys(this.busStops).length; this.i++) {
           if (this.busStops[this.i].toLowerCase().includes(this.searchInput.toLowerCase()) && this.tipsPostion < 5) {
             this.busStopsTips[this.tipsPostion++] = this.busStops[this.i];
           }
+          if (this.searchInput != '') {
+            this.showTips = true;
+            this.showTable = false;
+            if (this.tipsPostion > 0) {
+              this.inputClass = 'inputStyleActive';
+            } else {
+              this.inputClass = 'inputStyle';
+            }
+          } else {
+            this.showTips = false;
+            this.inputClass = 'inputStyle';
+          }
         }
       }
     }
-    
+
   }
 
 </script>
